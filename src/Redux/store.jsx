498315ed8da94f0221f6fcde1e4a3f6dcedbf551
sainath-07 <sainathb308@gmail.com>
2024-06-03 11:08:40 +0530

    successfully installed

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,4 +1,9 @@
 import { legacy_createStore} from 'redux'
 import { singleReducer } from './Redux-class-May28/Redux/combinereducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const storeRedux=legacy_createStore(singleReducer)
+
+export const storeRedux=legacy_createStore(
+    singleReducer,
+    composeWithDevTools()
+)
